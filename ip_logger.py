816bc0a724f0e6db84fa662052da5364f0eb645d
joejaden1 <joejaden92@gmail.com>
2025-06-17 13:59:37 +0000

    Add files via upload

---
+++ ip_logger.py
@@ -0,0 +1,20 @@
+# ip_logger.py
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_ip_info():
+    usrr_ip = request.remote_addr
+    ponse = requests.get(f"http://ip-api.com/json{user_ip}")
+    return f"""
+        <h1> done (; </h1>
+        <p>IP: {user_ip} </p>
+        <p>city: {response.get('city')}</p>
+        <p>country: {response.get('country')}</p>
+        <p>ISP: {response.get('isp')}</p>
+    """
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",port=5000)
