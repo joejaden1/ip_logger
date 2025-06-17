fab44fe5ccfafad1d0a5242d904cd3f7135c5fae
joejaden1 <joejaden92@gmail.com>
2025-06-17 14:19:18 +0000

    Update ip_logger.py

--- ip_logger.py
+++ ip_logger.py
@@ -1,4 +1,3 @@
-# ip_logger.py
 from flask import Flask, request
 import requests
 
@@ -7,14 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def get_ip_info():
     user_ip = request.remote_addr
-    response = requests.get(f"http://ip-api.com/json{user_ip}")
+    response = requests.get(f"http://ip-api.com/json/{user_ip}").json()  
+    
     return f"""
-        <h1> done (; </h1>
-        <p>IP: {user_ip} </p>
-        <p>city: {response.get('city')}</p>
-        <p>country: {response.get('country')}</p>
-        <p>ISP: {response.get('isp')}</p>
+        <h1>Done!</h1>
+        <p>IP: {user_ip}</p>
+        <p>City: {response.get('city', 'N/A')}</p>
+        <p>Country: {response.get('country', 'N/A')}</p>
+        <p>ISP: {response.get('isp', 'N/A')}</p>
     """
 
-if __name__ =="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
